8bb9c0419b2515dabf946abe826ac2c05c5e33f3
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-30 11:53:05 +0100

    :zap: command list

--- app.js
+++ app.js
@@ -6,13 +6,33 @@ const command = process.argv[2];
 
 yargs.version("1.1.0");
 
-// Create yargs commands
+// Create add command
 
 yargs.command({
   command: "add",
   describe: "Add a new note",
   handler: () => {
     console.log("Adding a new note!");
+  }
+});
+
+// Create remove command
+
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: () => {
+    console.log("Removing the note!");
+  }
+});
+
+// Create list command
+
+yargs.command({
+  command: "list",
+  describe: "List note",
+  handler: () => {
+    console.log("List the notes!");
   }
 });
 
