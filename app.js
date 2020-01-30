0e3caff0f26d1e71ad60bba2a3c6c36a25278f9d
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-30 11:49:11 +0100

    :zap: test yargs

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ yargs.version("1.1.0");
 
 // Create yargs commands
 
-yargs.command({});
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: () => {
+    console.log("Adding a new note!");
+  }
+});
 
 // add, remove, read, list
 
-console.log(process.argv);
 console.log(yargs.argv);
