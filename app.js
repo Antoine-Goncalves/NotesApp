4704a146ca94d28863594e72b98bdb412143ba85
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-31 08:43:18 +0100

    :rocket: add type of builder

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ yargs.command({
   builder: {
     title: {
       describe: "Note title",
-      demandOption: true
+      demandOption: true,
+      type: "String"
     }
   },
   handler: argv => {
